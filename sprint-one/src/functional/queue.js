b8521af7d83c433ace3edeeca38bcac8c360a2ce
Bobby Wei <showmethewei@gmail.com>
2016-09-14 12:33:16 -0700

    Implement queue using functional instantiation pattern, with object as backend

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -6,13 +6,24 @@ var Queue = function() {
 
   // Implement the methods below
 
+  var start = 0;
+  var end = 0; 
+
   someInstance.enqueue = function(value) {
+    storage[end] = value; // add something using the end key
+    end++;
   };
 
   someInstance.dequeue = function() {
+    if (start < end) {
+      start++;
+    }
+
+    return storage[start - 1]; // return whatever was at the start key before we incremented the counter
   };
 
   someInstance.size = function() {
+    return end - start;
   };
 
   return someInstance;
