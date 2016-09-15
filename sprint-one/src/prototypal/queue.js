5404b62689f16e60f48b771a01ada611160cbae9
cheneym <cheneymao94@gmail.com>
2016-09-15 08:56:00 -0700

    Implement stack and queue using prototypal instantiation

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,37 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // create instance by calling Object.create using an object of methods
+  var instance = Object.create(queueMethods);
+  // populate instance with properties, e.g. storage, start, finish
+  instance.storage = {};
+  instance.front = 0;
+  instance.back = 0;
+  /// return instance
+  return instance;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  // enqueue
+  enqueue: function(value) {
+    this.storage[this.back] = value;
+    this.back++;
+  },
+  // dequeue
+  dequeue: function() {
+    var result;
+    if (this.front < this.back) {
+      result = this.storage[this.front];
+      delete this.storage[this.front];
+      this.front++;
+    }
+    return result;
+  },
+  // size
+  size: function() {
+    return this.back - this.front;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,42 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  
+  //object create binding stackmethods
+  var instance = Object.create(stackMethods);
+
+  //add properties to object (counter, storage)
+  instance.counter = 0;
+  instance.storage = {};
+
+  //return object
+  return instance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+//push
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+
+//pop
+  pop: function() {
+    var results;
+    if (this.counter > 0) {
+      this.counter--;
+      results = this.storage[this.counter];
+      delete this.storage[this.counter];
+    }
+
+    return results;
+  },
+
+//size
+  size: function() {
+    return this.counter;
+  }
+};
+
 
 
