0bdad9b7b3c0be594e916b289ef433db366f2915
cheneym <cheneymao94@gmail.com>
2016-09-15 15:15:55 -0700

    Implement set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,28 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item) {
+  
+  //stringify item  
+  //put stringifyied item as key into _storage, with value true
+  this._storage[JSON.stringify(item)] = true;
 };
 
 setPrototype.contains = function(item) {
+  //Use stringified key to check if key exists in _storage
+
+  return !!this._storage[JSON.stringify(item)];
 };
 
 setPrototype.remove = function(item) {
+  //delete stringified item/value pair from _storage
+
+  delete this._storage[JSON.stringify(item)];
 };
 
 /*
