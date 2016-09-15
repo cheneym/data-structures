03efafd62ef1d8f78645e222c13af8a949d3e639
cheneym <cheneymao94@gmail.com>
2016-09-15 10:19:02 -0700

    Implement stack and queue using pseudoclassical instantiation

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,33 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // populate instance with required properties
+  this.storage = {};
+  this.front = 0;
+  this.back = 0;
 };
 
+// assign methods to Queue.prototype
 
+// enqueue
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.back] = value;
+  this.back++;
+};
+
+// dequeue
+Queue.prototype.dequeue = function() {
+  var result;
+  if (this.front < this.back) {
+    result = this.storage[this.front];
+    delete this.storage[this.front];
+    this.front++;
+  }
+  return result;
+};
+
+// size
+Queue.prototype.size = function() {
+  return this.back - this.front;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,36 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  //set properties by using this.prop ... (counter, storage)
+  this.counter = 0;
+  this.storage = {};
+};
+
+//add methods to stack's prototype
+
+// if you reassign constructor's prototype property to a new object, then you need to populate constructor 
+Stack.prototype = {
+  push: function(value) {
+    this.storage[this.counter] = value;
+    this.counter++;
+  },
+
+  pop: function() {
+    var results;
+    if (this.counter > 0) {
+      this.counter--;
+      results = this.storage[this.counter];
+      delete this.storage[this.counter];
+    }
+
+    return results;
+  },
+
+  size: function() {
+    return this.counter;
+  },
+
+  constructor: Stack // this actually works. constructor is just a reference to the function
 };
 
 
